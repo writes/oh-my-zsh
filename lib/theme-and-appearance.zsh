7a77aff11b7e54efcefe3f932e950c0a77453521
jona8578 <jonathon.thompson@rackspace.com>
2016-08-04 19:51:14 -0500

    changes

--- lib/theme-and-appearance.zsh
+++ lib/theme-and-appearance.zsh
@@ -34,6 +34,7 @@ fi
 
 # Apply theming defaults
 PS1="%n@%m:%~%# "
+# PS1="$ [%~] "
 
 # git theming default: Variables for theming the git info prompt
 ZSH_THEME_GIT_PROMPT_PREFIX="git:("         # Prefix at the very beginning of the prompt, before the branch name
